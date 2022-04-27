0dd609c80da38ceb3c84406267317d8b60ee6e95
Tringapps-Saranya <102800505+Tringapps-Saranya@users.noreply.github.com>
2022-04-27 15:12:52 +0530

    Update Auth.js

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -5,8 +5,8 @@ const AuthContext = createContext(null)
 export const AuthProvider = ({children}) =>{
     const[user,setUser] = useState(null)
 
-    const login = (user) =>{
-        setUser(user)
+    const login = (newuser) =>{
+        setUser(newuser)
     }
 
     const logout = () =>{
