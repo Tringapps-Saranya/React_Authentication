e4db6a344a4ab09768820d50185646cb83c2efb5
Tringapps-Saranya <102800505+Tringapps-Saranya@users.noreply.github.com>
2022-04-27 15:09:18 +0530

    Update Products.js

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,11 +1,10 @@
-import React from 'react'
-import { Link,Outlet} from 'react-router-dom'
-
 export const Products = () => {
   return (
-    <div>
-      <input type='search' placeholder='search products'></input>
-      <nav>
+    <div class="productdiv">
+      <div class="input">
+      <input type='search' placeholder='search products'></input><br></br>
+      </div>
+      <nav class="productnav">
         <Link id="linkkurti" to='kurtis'>Kurtis</Link>
         <Link id="linkwatch" to='watches'>Watches</Link>
       </nav>
